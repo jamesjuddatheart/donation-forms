d7449ea4fb225157b66315d2430bafbd4e1793fc
ironman67x <dean@dhwebworks.com>
2025-01-28 15:42:59 -0800

    Update donation.js

--- donatenow/js/donation.js
+++ donatenow/js/donation.js
@@ -492,6 +492,36 @@ jQuery(document).on("doublethedonation_company_id", function () {
 	jQuery("#double_the_donation_company_id").val(dtd_company_id);
 });
 
+jQuery('.donation-level-user-entered input').attr("placeholder","Amount").after("<div class='other-amt-note'><em>$25 minimum donation</em></div>");
+
+jQuery('.donation-level-container').click(function(){
+  jQuery('.donation-level-container').removeClass('active');
+  jQuery('.formMessage p').removeClass('active');
+  jQuery('.donation-level-container input[type=radio]').attr("aria-checked","false");
+  jQuery(this).find('input[type=radio]').attr("aria-checked","true");
+  jQuery(this).addClass('active');
+  for (var x=0;x<10;x++) {
+    if($(this).hasClass('level'+x)){
+       var level = 'level'+x;
+       if(jQuery('.donation-level-container.active').hasClass('level'+x)) {
+	   jQuery('.formMessage .level'+x).addClass('active');
+       }
+       break;
+    }
+  }
+
+  //check last entry to see if active
+  jQuery('.donation-level-user-entered').hide();
+  if(jQuery('.enterAmt.active').hasClass('level'+x)) {
+    jQuery('.donation-level-user-entered').show();
+  }
+}
+
+document.cookie="level="+level;
+//var amt = $(this).find('.donation-level-amount-container').text();
+//$('.donateSubmit').text('Donate '+amt);
+
+});
 /*
 $('#tributeType input[name^="year"]').click(function() {
   var radioval = $(this).val();
