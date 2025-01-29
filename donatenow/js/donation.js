de363ebfe419f17e4647c76ddf059f2a8f20a3b3
ironman67x <dean@dhwebworks.com>
2025-01-28 16:12:52 -0800

    Update donation.js

--- donatenow/js/donation.js
+++ donatenow/js/donation.js
@@ -492,7 +492,7 @@ jQuery(document).on("doublethedonation_company_id", function () {
 	jQuery("#double_the_donation_company_id").val(dtd_company_id);
 });
 
-
+console.log("apply donation updates");
 jQuery('.donation-level-user-entered input').attr("placeholder","Amount").after("<div class='other-amt-note'><em>$25 minimum donation</em></div>");
 
 jQuery('.donation-level-container:last-child').addClass('enterAmt');
